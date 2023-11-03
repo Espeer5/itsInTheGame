194e11b90f7c3c4e2a4ff38ae4a66c66571789f6
AndrewPasco <apascos@gmail.com>
2023-11-02 22:13:26 -0700

    added harmonic mean util

--- src/distinguish/utils.py
+++ src/distinguish/utils.py
@@ -20,6 +20,17 @@ def euc_dist(p1, p2):
     """Computes the euclidean distance between two points"""
     return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**(1/2)
 
+def H_mean(points):
+    '''Computes the harmonic mean of a number of points.
+    Harmonic mean is dominated by the minimum of its arguments,
+    which will help with maximally distinguishing all four points.'''
+    x = 0
+    y = 0
+    for p in points:
+        x += 1/p[0]
+        y += 1/p[1]
+    return (len(points) / x, len(points) / y)
+
 
 def disting(points):
     """Computes the distinguishability of a set of points given by 
