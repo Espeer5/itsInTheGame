40f661c8406bbce3d166c3511f292ae0061df449
Edward Speer <espeer@caltech.edu>
2023-11-22 20:12:49 -0800

    simple level 1 solver

---
+++ src/solvers/level1.py
@@ -0,0 +1,33 @@
+"""This solver is a level one model in which players throughout the population 
+play as a best response to a level 0 distribution. Here level 0 is assigned to 
+be (.5, .5). The best response is given by the same as the first step in Poisson
+CH solver.
+"""
+
+def level_1(game):
+    """Returns the p and q values for a given game as a best response to level 
+    0 (random mixing) play."""
+    p_0 = [(0.5, 0.5)]
+    q_0 = [(0.5, 0.5)]
+
+    # Compute row probabilities for level 1
+    E_R_0 = sum([game.get_row_matrix()[0][c] * q_0[0][c] for c in range(0, 2)])
+    E_R_1 = sum([game.get_row_matrix()[1][c] * q_0[0][c] for c in range(0, 2)])
+    if E_R_0 > E_R_1:
+        p = (1, 0)
+    elif E_R_0 < E_R_1:
+        p = (0, 1)
+    else:
+        p = (0.5, 0.5)
+
+    # Compute column probabilities for level 1
+    E_C_0 = sum([game.get_col_matrix()[r][0] * p_0[0][0] for r in range(0, 2)])
+    E_C_1 = sum([game.get_col_matrix()[r][1] * p_0[0][1] for r in range(0, 2)])
+    if E_C_0 > E_C_1:
+        q = (1, 0)
+    elif E_C_0 < E_C_1:
+        q = (0, 1)
+    else:
+        q = (0.5, 0.5)
+
+    return p, q
